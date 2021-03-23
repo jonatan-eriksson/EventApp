37041655f11da536bd7f00df10449e384f9918b9
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-03-23 20:31:52 +0100

    Add data seeding

---
+++ EventApp/Data/DbInitializer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+using EventApp.Models;
+
+namespace EventApp.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(EventContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Events.Any())
+            {
+                return;
+            }
+
+            var organizer = new Organizer()
+            {
+                Name = "Organizer",
+                Email = "organizer@events.com",
+                PhoneNumber = "0000"
+            };
+            context.Organizers.Add(organizer);
+            context.SaveChanges();
+
+            var attendees = new Attendee()
+            {
+                Name = "John Doe",
+                Email = "john.doe@abc.com",
+                PhoneNumber = "0001"
+            };
+
+            context.Attendees.AddRange(attendees);
+            context.SaveChanges();
+
+            var events = new Event[]
+            {
+                new Event{ Title="Event 1", Description="test", Place="Arena", Address="Abc 123", Date=DateTime.Parse("2021-05-01"), SpotsAvailable=10, Organizer=organizer},
+                new Event{ Title="Event 2", Description="test", Place="Club", Address="Abc 123", Date=DateTime.Parse("2021-06-01"), SpotsAvailable=20, Organizer=organizer},
+                new Event{ Title="Event 3", Description="test", Place="Arena", Address="Abc 123", Date=DateTime.Parse("2021-07-01"), SpotsAvailable=30, Organizer=organizer}
+            };
+            context.Events.AddRange(events);
+            context.SaveChanges();
+        }
+    }
+}
--- EventApp/Program.cs
+++ EventApp/Program.cs
@@ -1,5 +1,7 @@
+using EventApp.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,13 +9,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//  dotnet aspnet-codegenerator razorpage Events List -m Event - -dc EventApp.Data.EventContext -outDir Pages/Events
 namespace EventApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<EventContext>();
+                    // Initialize DB
+                    DbInitializer.Initialize(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
