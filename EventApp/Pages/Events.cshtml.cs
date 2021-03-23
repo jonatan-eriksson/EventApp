661fb2d8f7b7da6034e26bd5c064538c8474dc23
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-03-23 20:33:00 +0100

    Add page to list all available events

---
+++ EventApp/Pages/Events.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EventApp.Data;
+using EventApp.Models;
+
+namespace EventApp.Pages
+{
+    public class EventsModel : PageModel
+    {
+        private readonly EventApp.Data.EventContext _context;
+
+        public EventsModel(EventApp.Data.EventContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Event = await _context.Events
+                .Include(e => e.Organizer).ToListAsync();
+        }
+    }
+}
