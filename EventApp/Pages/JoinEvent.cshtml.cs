cde3bf37dd8582a72f7a6c49cc38cc7230e0d6f1
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-03-24 15:55:22 +0100

    Add join button to allow users to join events

--- EventApp/Pages/JoinEvent.cshtml.cs
+++ EventApp/Pages/JoinEvent.cshtml.cs
@@ -12,14 +12,16 @@ namespace EventApp.Pages
 {
     public class JoinEventModel : PageModel
     {
-        private readonly EventApp.Data.EventContext _context;
+        private readonly EventContext _context;
 
-        public JoinEventModel(EventApp.Data.EventContext context)
+        public JoinEventModel(EventContext context)
         {
             _context = context;
         }
 
         public Event Event { get; set; }
+        public Attendee Attendee { get; set; }
+        public bool HasJoined { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -28,14 +30,35 @@ namespace EventApp.Pages
                 return NotFound();
             }
 
-            Event = await _context.Events
-                .Include(e => e.Organizer).FirstOrDefaultAsync(m => m.Id == id);
+            Attendee = await _context.Attendees.FirstOrDefaultAsync();
+            Event = await _context.Events.Include(e => e.Attendees).FirstOrDefaultAsync(m => m.Id == id);
+            HasJoined = Event.Attendees.Contains(Attendee);
 
             if (Event == null)
             {
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Attendee = await _context.Attendees.FirstOrDefaultAsync();
+            Event = await _context.Events.Include(e => e.Attendees).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event.Attendees == null)
+                Event.Attendees = new List<Attendee>();
+
+            if (!Event.Attendees.Contains(Attendee))
+            {
+                Event.Attendees.Add(Attendee);
+                Event.SpotsAvailable--;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage(new { id });
         }
     }
 }
